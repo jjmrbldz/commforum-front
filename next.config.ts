df99ef057b0b0aa37ad1093ca1e64b59ab1ad437
unknown <jejo.baldoza@gmail.com>
2025-07-29 21:48:30 +0800

    remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/user',
-  assetPrefix: '/user',
-  trailingSlash: true,
 };
 
 export default nextConfig;
