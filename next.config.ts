b324f95a0cfe56d809388758a973d3c037ec1767
unknown <jejo.baldoza@gmail.com>
2025-07-29 19:56:07 +0800

    add base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/user',
+  assetPrefix: '/user',
 };
 
 export default nextConfig;
