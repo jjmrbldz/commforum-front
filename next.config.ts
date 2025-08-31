63a6646f95711abdf31ea75fd2e8a32226e52811
unknown <jejo.baldoza@gmail.com>
2025-09-01 07:14:51 +0800

    add image remote patters

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'tazza365.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'www.tazza365.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tazza365.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.tazza365.com',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
